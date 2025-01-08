9c0777815ccbaf4d83a026ed235e49f8cc4a1b4f
Luis Felipe <felipenarciso15@gmail.com>
2025-01-08 16:46:33 -0300

    barra de informação

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { HeroComponent } from '../hero/hero.component';
+import { InformationBarComponent } from '../information-bar/information-bar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent, InformationBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/components/information-bar/information-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-bar',
+  imports: [],
+  templateUrl: './information-bar.component.html',
+  styleUrl: './information-bar.component.css'
+})
+export class InformationBarComponent {
+
+}
